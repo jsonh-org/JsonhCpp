cdbcf5e6c4b270530cd0e9194bff5781442f35a8
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-04-15 02:44:16 +0100

    Add doc comments for options

--- jsonh_cpp/jsonh_reader_options.hpp
+++ jsonh_cpp/jsonh_reader_options.hpp
@@ -2,7 +2,19 @@
 
 namespace jsonh {
 
+/// <summary>
+/// Options for a <see cref="jsonh_reader"/>.
+/// </summary>
 struct jsonh_reader_options final {
+    /// <summary>
+    /// Enables/disables parsing unclosed inputs.
+    /// <code>
+    /// {
+    ///   "key": "val
+    /// </code>
+    /// This is potentially useful for large language models that stream responses.<br/>
+    /// Only some tokens can be incomplete in this mode, so it should not be relied upon.
+    /// </summary>
     bool incomplete_inputs = false;
 };
 
