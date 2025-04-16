6c088f91e3dbddb16f627b9c8a18ad9b81d58848
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-04-17 00:57:27 +0100

    Finish tests

--- jsonh_cpp_tests/jsonh_cpp_tests.cpp
+++ jsonh_cpp_tests/jsonh_cpp_tests.cpp
@@ -7,105 +7,116 @@
 
 using namespace jsonh;
 
-const std::string kanji_character = "\u79C1"; // 私
+/*
+    Read Tests
+*/
 
-TEST_CASE("Basic Test") {
-    std::cout << jsonh_reader("'hello'").parse_element().value() << '\n';
-    /*for (std::expected<jsonh_token, std::string> v : jsonh_reader("'hello'").read_element()) {
-        std::cout << (int)v.value().json_type << '\n';
-        std::cout << v.value().value << '\n';
-    }*/
-
-
+TEST_CASE("BasicObjectTest") {
     std::string jsonh = R"(
-6 ab a
+{
+    "a": "b"
+}
 )";
-    std::string element = jsonh::jsonh_reader(jsonh).parse_element<std::string>().value();
-    std::cout << element << '\n';
+    jsonh_reader reader(jsonh);
+    std::vector<std::expected<jsonh_token, std::string>> tokens = reader.read_element();
+
+    for (const std::expected<jsonh_token, std::string>& token : tokens) {
+        REQUIRE(token);
+    }
+    REQUIRE(tokens[0].value().json_type == json_token_type::start_object);
+    REQUIRE(tokens[1].value().json_type == json_token_type::property_name);
+    REQUIRE(tokens[1].value().value == "a");
+    REQUIRE(tokens[2].value().json_type == json_token_type::string);
+    REQUIRE(tokens[2].value().value == "b");
+    REQUIRE(tokens[3].value().json_type == json_token_type::end_object);
 }
 
-TEST_CASE("Unicode Escape Sequences") {
-    REQUIRE(jsonh_reader("'\\u00E7'").parse_element().value() == "ç");
-    REQUIRE(jsonh_reader("'\\xE7'").parse_element().value() == "ç");
-    REQUIRE(jsonh_reader("'\\U0001F47D'").parse_element().value() == "👽");
-}
+/*
+    Parse Tests
+*/
 
-TEST_CASE("Comments") {
-    jsonh_reader reader(R"(
-// line comment
-/* block comment */
-/* multiline
-block comment */
-)");
-    auto tokens = reader.read_element();
-    REQUIRE(tokens[0].value().value == " line comment");
-    REQUIRE(tokens[1].value().value == " block comment ");
-    REQUIRE(tokens[2].value().value == R"( multiline
-block comment )");
-}
+TEST_CASE("EscapeSequenceTest") {
+    std::string jsonh = R"(
+"\U0001F47D and \uD83D\uDC7D"
+)";
+    std::string element = jsonh_reader::parse_element<std::string>(jsonh).value();
 
+    REQUIRE(element == "👽 and 👽");
+}
+TEST_CASE("QuotelessEscapeSequenceTest") {
+    std::string jsonh = R"(
+\U0001F47D and \uD83D\uDC7D
+)";
+    std::string element = jsonh_reader::parse_element<std::string>(jsonh).value();
+
+    REQUIRE(element == "👽 and 👽");
+}
+TEST_CASE("MultiQuotedStringTest") {
+    std::string jsonh = R"(
+""""
+Hello!Here's a quote: ". Now a double quote: "". And a triple quote! """. Escape: \\\U0001F47D.
+""""
+)";
+    std::string element = jsonh_reader::parse_element<std::string>(jsonh).value();
+
+    REQUIRE(element == " Hello! Here's a quote: \". Now a double quote: \"\". And a triple quote! \"\"\". Escape: \\👽.");
+}
+TEST_CASE("ArrayTest") {
+    std::string jsonh = R"(
+[
+    1, 2,
+    3
+    4 5, 6
+]
+)";
+    json element = jsonh_reader::parse_element(jsonh).value();
+
+    REQUIRE(element.size() == 5);
+    REQUIRE(element[0] == 1);
+    REQUIRE(element[1] == 2);
+    REQUIRE(element[2] == 3);
+    REQUIRE(element[3] == "4 5");
+    REQUIRE(element[4] == 6);
+}
 TEST_CASE("NumberParserTest") {
     REQUIRE((int)jsonh_number_parser::parse("1.2e3.4").value() == 3014);
 }
+TEST_CASE("BracelessObjectTest") {
+    std::string jsonh = R"(
+a: b
+c : d
+)";
+    json element = jsonh_reader::parse_element(jsonh).value();
 
-TEST_CASE("Test 1") {
-    jsonh_reader reader(R"(
-// hello
-/* hi */
-aaa
-)");
-    for (std::expected<jsonh_token, std::string>& token : reader.read_element()) {
-        REQUIRE(token);
-    }
+    REQUIRE(element.size() == 2);
+    REQUIRE(element["a"] == "b");
+    REQUIRE(element["c"] == "d");
+}
+TEST_CASE("CommentTest") {
+    std::string jsonh = R"(
+[
+    1 # hash comment
+        2 // line comment
+        3 /* block comment */, 4
+]
+)";
+    std::vector<int> element = jsonh_reader::parse_element<std::vector<int>>(jsonh).value();
+
+    REQUIRE(element.size() == 4);
+    REQUIRE(element[0] == 1);
+    REQUIRE(element[1] == 2);
+    REQUIRE(element[2] == 3);
+    REQUIRE(element[3] == 4);
 }
 
-TEST_CASE("Test 2") {
-    jsonh_reader reader(R"(
-// hello
-/w hi
-aaa
-)");
-    bool found_error = false;
-    for (std::expected<jsonh_token, std::string>& token : reader.read_element()) {
-        if (!token) {
-            found_error = true;
-            break;
-        }
-    }
-    if (!found_error) {
-        REQUIRE(false);
-    }
-}
-
-TEST_CASE("string") {
-    jsonh_reader reader(kanji_character);
-    REQUIRE(reader.parse_element<std::string>() == kanji_character);
-}
-
-TEST_CASE("qstring") {
-    jsonh_reader reader("a b ");
-    REQUIRE(reader.parse_element<std::string>() == "a b");
-}
-
-/*TEST_CASE("u8string") {
-    jsonh_reader reader(utf8::utf32tou8(utf8::utf8to32(std::string(unicode_character))));
-    REQUIRE(reader.parse_element<std::string>() == unicode_character);
-}
-
-TEST_CASE("u16string") {
-    jsonh_reader reader(utf8::utf8to16(std::string(unicode_character)));
-    REQUIRE(reader.parse_element<std::string>() == unicode_character);
-}
-
-TEST_CASE("u32string") {
-    jsonh_reader reader(utf8::utf8to32(std::string(unicode_character)));
-    REQUIRE(reader.parse_element<std::string>() == unicode_character);
-}*/
+/*
+    Edge Case Tests
+*/
 
 TEST_CASE("QuotelessStringStartingWithKeywordTest") {
-    std::string jsonh = ""
-        "[nulla, null b, null]"
-        "";
+    std::string jsonh = R"(
+[nulla, null b, null]
+)";
     std::vector<json> element = jsonh_reader::parse_element<std::vector<json>>(jsonh).value();
 
     REQUIRE(element.size() == 3);
@@ -113,62 +124,71 @@ TEST_CASE("QuotelessStringStartingWithKeywordTest") {
     REQUIRE(element[1] == "null b");
     REQUIRE(element[2] == nullptr);
 }
-/*TEST_CASE("BracelessObjectWithInvalidValueTest") {
-    string Jsonh = """
-        a: {
-        """;
-    JsonhReader.ParseElement<string[]>(Jsonh).IsError.ShouldBeTrue();
+TEST_CASE("BracelessObjectWithInvalidValueTest") {
+    std::string jsonh = R"(
+a: {
+)";
+
+    REQUIRE(!jsonh_reader::parse_element(jsonh));
 }
 TEST_CASE("NestedBracelessObjectTest") {
-    string Jsonh = """
-        [
-            a: b
-            c: d
-        ]
-        """;
-    JsonhReader.ParseElement<string[]>(Jsonh).IsError.ShouldBeTrue();
+    std::string jsonh = R"(
+[
+    a: b
+    c: d
+]
+)";
+
+    REQUIRE(!jsonh_reader::parse_element<std::vector<std::string>>(jsonh));
 }
 TEST_CASE("QuotelessStringsLeadingTrailingWhitespaceTest") {
-    string Jsonh = """
-        [
-            a b  , 
-        ]
-        """;
-    JsonhReader.ParseElement<string[]>(Jsonh).Value.ShouldBe(["a b"]);
+    std::string jsonh = R"(
+[
+    a b  , 
+]
+)";
+    std::vector<std::string> element = jsonh_reader::parse_element<std::vector<std::string>>(jsonh).value();
+
+    REQUIRE(element.size() == 1);
+    REQUIRE(element[0] == "a b");
 }
 TEST_CASE("SpaceInQuotelessPropertyNameTest") {
-    string Jsonh = """
-        {
-            a b: c d
-        }
-        """;
-    JsonElement Element = JsonhReader.ParseElement(Jsonh).Value;
-    Element.GetPropertyCount().ShouldBe(1);
-    Element.GetProperty("a b").Deserialize<string>(JsonhReader.MiniJson).ShouldBe("c d");
+    std::string jsonh = R"(
+{
+    a b: c d
+}
+)";
+    json element = jsonh_reader::parse_element(jsonh).value();
+
+    REQUIRE(element.size() == 1);
+    REQUIRE(element["a b"] == "c d");
 }
 TEST_CASE("QuotelessStringsEscapeTest") {
-    string Jsonh = """
-        a: \"5
-        b: \\z
-        c: 5 \\
-        """;
-    JsonElement Element = JsonhReader.ParseElement(Jsonh).Value;
-    Element.GetPropertyCount().ShouldBe(3);
-    Element.GetProperty("a").Deserialize<string>(JsonhReader.MiniJson).ShouldBe("\"5");
-    Element.GetProperty("b").Deserialize<string>(JsonhReader.MiniJson).ShouldBe("\\z");
-    Element.GetProperty("c").Deserialize<string>(JsonhReader.MiniJson).ShouldBe("5 \\");
+    std::string jsonh = R"(
+a: \"5
+b: \\z
+c: 5 \\
+)";
+    json element = jsonh_reader::parse_element(jsonh).value();
+
+    REQUIRE(element.size() == 3);
+    REQUIRE(element["a"] == "\"5");
+    REQUIRE(element["b"] == "\\z");
+    REQUIRE(element["c"] == "5 \\");
 }
 TEST_CASE("MultiQuotedStringsNoLastNewlineWhitespaceTest") {
-    string Jsonh = """"
-        """
-            hello world  """
-        """";
-    JsonhReader.ParseElement(Jsonh).Value.Deserialize<string>(JsonhReader.MiniJson).ShouldBe("\n  hello world  ");
+    std::string jsonh = R"(
+"""
+  hello world  """
+)";
+
+    REQUIRE(jsonh_reader::parse_element<std::string>(jsonh).value() == "\n  hello world  ");
 }
 TEST_CASE("MultiQuotedStringsNoFirstWhitespaceNewlineTest") {
-    string Jsonh = """"
-        """  hello world
-            """
-        """";
-    JsonhReader.ParseElement(Jsonh).Value.Deserialize<string>(JsonhReader.MiniJson).ShouldBe("  hello world\n  ");
-}*/
+    std::string jsonh = R"(
+"""  hello world
+  """
+)";
+
+    REQUIRE(jsonh_reader::parse_element<std::string>(jsonh).value() == "  hello world\n  ");
+}
