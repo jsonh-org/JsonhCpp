3e77458157031fa449c64274c35d6d5c3bf761ed
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-04-18 04:24:33 +0100

    Remove unnecessary includes

--- jsonh_cpp/jsonh_cpp.hpp
+++ jsonh_cpp/jsonh_cpp.hpp
@@ -1,9 +1,3 @@
 #pragma once
 
-#include "jsonh_token_type.hpp"
-#include "jsonh_token.hpp"
 #include "jsonh_reader.hpp"
-#include "jsonh_reader_options.hpp"
-#include "jsonh_number_parser.hpp"
-#include "utf8_reader.hpp"
-#include "nlohmann/json.hpp"
