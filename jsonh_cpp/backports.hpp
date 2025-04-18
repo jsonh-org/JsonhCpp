91a45537045f9555729d845bdf933810b32e6817
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-04-18 13:10:37 +0100

    Simplify backports.hpp

--- jsonh_cpp/backports.hpp
+++ jsonh_cpp/backports.hpp
@@ -1,10 +1,9 @@
-// <expected> backport
+// <expected> backport from C++23 to C++20
 #ifdef __cpp_lib_expected
 #include <expected> // for std::expected
 #define JSONH_CPP_EXPECTED std::expected
 #define JSONH_CPP_UNEXPECTED std::unexpected
-#endif
-#ifndef __cpp_lib_expected
+#else
 #include "martinmoene/expected.hpp" // for nonstd::expected
 #define JSONH_CPP_EXPECTED nonstd::expected
 #define JSONH_CPP_UNEXPECTED nonstd::unexpected
