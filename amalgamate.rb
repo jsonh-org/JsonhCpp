509f9afa8db7c7cc056ecbf4d5424aa2009799c9
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-05-18 03:30:41 +0100

    Improve amalgamation

---
+++ amalgamate.rb
@@ -0,0 +1,17 @@
+AMALGAMATE_PATH = 'C:/Users/patri/Documents/Programs/amalgamate-v0.99.0-windows-amd64/amalgamate.exe' # https://github.com/rindeal/Amalgamate
+INPUT_PATH = "jsonh_cpp/jsonh_cpp.hpp"
+OUTPUT_PATH = "single_include/jsonh_cpp_amalgamated.hpp"
+
+HEADER = "// JsonhCpp (JSON for Humans)
+// Version: 4.2
+// Link: https://github.com/jsonh-org/JsonhCpp
+// License: MIT"
+
+# Amalgamate source to single file
+system("#{AMALGAMATE_PATH} \"#{INPUT_PATH}\" \"#{OUTPUT_PATH}\"")
+
+# Prepend header
+File.write(OUTPUT_PATH, HEADER + "\n\n" + File.read(OUTPUT_PATH))
+
+puts("All done!")
+gets()
--- bat_amalgamate.bat
+++
@@ -1,6 +0,0 @@
-:: Prevent quit on error
-if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit
-
-:: Amalgamate with https://github.com/rindeal/Amalgamate
-set amalgamate_path="C:/Users/patri/Documents/Programs/amalgamate-v0.99.0-windows-amd64/amalgamate.exe"
-%amalgamate_path% "jsonh_cpp/jsonh_cpp.hpp" "jsonh_cpp_amalgamated.hpp"
